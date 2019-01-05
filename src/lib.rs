309d64a6872506291c0eca17a3ab7b08ac4fb914
Fedor Indutny <fedor@indutny.com>
2019-01-05 19:59:29 +0100

    src: port to newest dumb-crypto

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,14 @@
 extern crate dumb_crypto;
 extern crate wasm_bindgen;
 
-mod utils;
-
-use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
-
-cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
 
 #[wasm_bindgen]
 pub fn derive(r: usize, n: usize, p: usize, passphrase: &str, salt: &str,
               out_size: usize) -> Vec<u8> {
     let mut out: Vec<u8> = vec![0; out_size];
 
-    let s = dumb_crypto::scrypt::Scrypt::new(r, n, p);
+    let s = dumb_crypto::scrypt::Scrypt::new(r, n, p).unwrap();
 
     s.derive(passphrase.as_bytes(), salt.as_bytes(), &mut out);
 
