baa6135af9e5ef4ae7c061a0b95e1e1f8c7b3f48
Fedor Indutny <fedor@indutny.com>
2019-01-05 15:14:41 +0100

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# dumb-scrypt-wasm
+
+This is a wrapper library for compiling [dumb-crypto][dumb]'s `scrypt` method
+to [WebAssembly][rustwasm].
+
+## Why?
+
+WIP
+
+## Quick example
+
+WIP
+
+[dumb]: https://github.com/indutny/dumb-crypto/
+[rustwasm]: https://rustwasm.github.io/book/
---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+extern crate dumb_crypto;
+extern crate wasm_bindgen;
+
+mod utils;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+pub fn derive(r: usize, n: usize, p: usize, passphrase: &str, salt: &str,
+              out_size: usize) -> Vec<u8> {
+    let mut out: Vec<u8> = vec![0; out_size];
+
+    let s = dumb_crypto::scrypt::Scrypt::new(r, n, p);
+
+    s.derive(passphrase.as_bytes(), salt.as_bytes(), &mut out);
+
+    out
+}
